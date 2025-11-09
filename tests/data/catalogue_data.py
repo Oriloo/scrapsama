b61aa14d2a445b64549b12bcf0f70f63daa2d6ad
Oriloo <112803541+Oriloo@users.noreply.github.com>
2025-11-09 16:30:52 +0100

    Update URLs to point to anime-sama.org

--- tests/data/catalogue_data.py
+++ tests/data/catalogue_data.py
@@ -1,6 +1,6 @@
 from anime_sama_api.catalogue import Catalogue
 
 
-one_piece = Catalogue("https://anime-sama.fr/catalogue/one-piece/")
-mha = Catalogue("https://anime-sama.fr/catalogue/my-hero-academia/")
-gumball = Catalogue("https://anime-sama.fr/catalogue/le-monde-incroyable-de-gumball/")
+one_piece = Catalogue("https://anime-sama.org/catalogue/one-piece/")
+mha = Catalogue("https://anime-sama.org/catalogue/my-hero-academia/")
+gumball = Catalogue("https://anime-sama.org/catalogue/le-monde-incroyable-de-gumball/")
